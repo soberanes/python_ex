87d7b431ed584d53fce7d95464d6b8d353b3f28b
Paul Soberanes <paul.soberanes@omnilife.com>
2017-10-03 12:08:21 -0500

    Dictionary: Key-Value Data Structure

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+dictionary_tk = {
+  "name": "Leandro",
+  "nickname": "Tk",
+  "nationality": "Brazilian"
+}
+dictionary_tk["age"] = 24
+
+print("My name is %s" %(dictionary_tk["name"])) # My name is Leandro
+print("But you can call me %s" %(dictionary_tk["nickname"])) # But you can call me Tk
+print("And by the way I'm %i and %s" %(dictionary_tk["age"], dictionary_tk["nationality"])) # And by the way I'm Brazilian
+print(dictionary_tk)
