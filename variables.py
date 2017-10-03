aadf7428154629224f75ec0c251e468d03f43039
Paul Soberanes <paul.soberanes@omnilife.com>
2017-10-03 11:42:23 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+two = 2
+some_number = 10000
+
+# booleans
+true_boolean = True
+false_boolean = False
+
+# strings
+my_name = "Paul Soberanes"
+
+# float
+book_price = 15.80
