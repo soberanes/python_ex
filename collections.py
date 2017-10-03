c7965c0ce26ba8f6ab69072ff90db309d647e8fa
Paul Soberanes <paul.soberanes@omnilife.com>
2017-10-03 12:01:06 -0500

    List Collections

---
+++ collections.py
@@ -0,0 +1,20 @@
+my_integers = [5, 7, 1, 3, 4]
+print(my_integers[0]) # 5
+print(my_integers[1]) # 7
+print(my_integers[4]) # 4
+
+
+relatives_names = [
+    "Toshiaki",
+    "Juliana",
+    "Yuji",
+    "Bruno",
+    "Kaio"
+]
+print(relatives_names[4]) # Kaio
+
+bookshelf = []
+bookshelf.append("The Effective Engineer")
+bookshelf.append("The 4 Hour Work Week")
+print(bookshelf[0]) # The Effective Engineer
+print(bookshelf[1]) # The 4 Hour Work Week
